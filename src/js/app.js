ee89f88064d3cb66e7ca9c60c3b812822053177b
Shalaevskiy Nikolay <nshalaevskiy@intraservices.ru>
2024-08-15 16:38:49 +0300

    sec

--- src/js/app.js
+++ src/js/app.js
@@ -1,86 +1,38 @@
 import Character from './character';
 
 export class Bowman extends Character {
-<<<<<<< HEAD
   constructor(name, health, level) {
     super(name, 'Bowman', health, level, 25, 25);
-=======
-  constructor(name, health, level, attack = 25, defence = 25) {
-    super(name, health, level);
-    this.type = 'Bowman';
-    this.attack = attack;
-    this.defence = defence;
->>>>>>> 12574c8c46e468298d17e8a2b48f919319adf47d
   }
 }
 
 export class Swordsman extends Character {
-<<<<<<< HEAD
   constructor(name, health, level) {
     super(name, 'Swordsman', health, level, 40, 10);
-=======
-  constructor(name, health, level, attack = 40, defence = 10) {
-    super(name, health, level);
-    this.type = 'Swordsman';
-    this.attack = attack;
-    this.defence = defence;
->>>>>>> 12574c8c46e468298d17e8a2b48f919319adf47d
   }
 }
 
 export class Magician extends Character {
-<<<<<<< HEAD
   constructor(name, health, level) {
     super(name, 'Magician', health, level, 10, 40);    
-=======
-  constructor(name, health, level, attack = 10, defence = 40) {
-    super(name, health, level);
-    this.type = 'Magician';
-    this.attack = attack;
-    this.defence = defence;
->>>>>>> 12574c8c46e468298d17e8a2b48f919319adf47d
   }
 }
 
 export class Daemon extends Character {
-<<<<<<< HEAD
   constructor(name, health, level) {
     super(name, 'Daemon', health, level, 10, 40);  
-=======
-  constructor(name, health, level, attack = 10, defence = 40) {
-    super(name, health, level);
-    this.type = 'Daemon';
-    this.attack = attack;
-    this.defence = defence;
->>>>>>> 12574c8c46e468298d17e8a2b48f919319adf47d
   }
 }
 
 export class Undead extends Character {
-<<<<<<< HEAD
   constructor(name, health, level) {
     super(name, 'Undead', health, level, 25, 25);
-=======
-  constructor(name, health, level, attack = 25, defence = 25) {
-    super(name, health, level);
-    this.type = 'Undead';
-    this.attack = attack;
-    this.defence = defence;
->>>>>>> 12574c8c46e468298d17e8a2b48f919319adf47d
   }
 }
 
 export class Zombie extends Character {
-<<<<<<< HEAD
   constructor(name, health, level) {
     super(name, 'Zombie', health, level, 40, 10);
-=======
-  constructor(name, health, level, attack = 40, defence = 10) {
-    super(name, health, level);
-    this.type = 'Zombie';
-    this.attack = attack;
-    this.defence = defence;
->>>>>>> 12574c8c46e468298d17e8a2b48f919319adf47d
   }
 }
 
